888767258725b9076a3bdad356cf1e9d9026501f
David MacEachern <david.maceachern.tech@gmail.com>
2020-10-02 17:41:30 +0200

    initial commit

---
+++ configuration.nix
@@ -0,0 +1,67 @@
+{ config, libs, pkgs, ... }:
+
+{
+  imports =
+    [ 
+      ./hardware-configuration.nix
+    ];
+  system.stateVersion = "20.09";
+  boot = {
+    loader.grub.enable = false;
+    loader.raspberryPi.enable = true;
+    loader.raspberryPi.version = 4;
+    kernelPackages = pkgs.linuxPackages_rpi4;
+  };
+  networking = {
+    hostName = "nixos";
+    useDHCP = false;
+    interfaces.eth0.useDHCP = true;
+    interfaces.wlan0.useDHCP = true;
+  };
+  services.openssh = {
+    enable = true;
+    permitRootLogin = "yes";
+    passwordAuthentication = true;
+    challengeResponseAuthentication = false;
+    authorizedKeysFiles = [ "/etc/ssh/authorized_keys.d/nixos" ];
+  };
+  systemd.services.sshd.wantedBy = pkgs.lib.mkForce [ "multi-user.target" ];
+  users.users.nixpi = {
+    isNormalUser = true;
+    home = "/home/nixpi";
+    extraGroups = [ "wheel" "networkmanager" ];
+    openssh.authorizedKeys.keys = [ "" ];
+  };
+  # Auto GC every morning
+  nix.gc.automatic = false;
+  services.cron.systemCronJobs = [ "0 3 * * * root /etc/admin/optimize-nix" ];
+  environment.etc =
+  {
+    "admin/optimize-nix" =
+    {
+      text =
+      ''
+        #!/run/current-system/sw/bin/bash
+        set -eu
+
+        # Delete everything from this profile that isn't currently needed
+        nix-env --delete-generations old
+
+        # Delete generations older than a week
+        nix-collect-garbage
+        nix-collect-garbage --delete-older-than 7d
+
+        # Optimize
+        nix-store --gc --print-dead
+        nix-store --optimise
+      '';
+      mode = "0774";
+    };
+  };
+  #####################
+  # System Applications
+  #####################
+  environment.systemPackages = with pkgs; [
+    git
+  ];
+}

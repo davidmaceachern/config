e2523f466a4e2b59f5ff07ddd95c681777b6a3d9
David MacEachern <david.maceachern.tech@gmail.com>
2020-10-05 18:39:09 +0200

    Add zsh

---
+++ aliases.nix
@@ -0,0 +1,10 @@
+{
+  git-nevermind = "git-abort ; git reset . ; git checkout . ; git clean -f -d";
+  ls = "ls --color=auto -F";
+  u = "cd ..";
+  uu = "cd ../..";
+  uuu = "cd ../../..";
+  uuuu = "cd ../../../..";
+  uuuuu = "cd ../../../../..";
+  uuuuuu = "cd ../../../../../..";
+}
--- bootstrap.configuration.nix
+++ bootstrap.configuration.nix
@@ -52,19 +52,34 @@
       mode = "0774";
     };
   };
-  users.users.davidmaceachern = {
-    description = "David MacEachern";
-    isNormalUser = true;
-    createHome = true;
-    home = "/home/davidmaceachern";
-    extraGroups = [ "wheel" "networkmanager"];
-    openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDED+Kh6L67xD79Dzh/n3sjC5OLMba9xphRktLMm14xATI6aH4miXwI2kstn7bhy/16qcLthdv3hMvAooSy4kIfkWSa8MURGX/I5CUCgJrZuNH2KkUP+fqi7tcz9z6PS+0mAJR097rj7F+zDe+gDAibJ6vex7TMjyKVOmzNlaRlcJl2Bza0ddA5EJ3nJHXJGh0cvwTMVLw0JbRhKKz9s87gywVzhgBBVIZLXxu4Aliku91A5xCFPWWdEPXdkrH4RTN7qM2/bIGs6cUVtJ8lcEZU4+z6dhYb4Fu/z873aPrcoOp9ufsGQqCGWoobCIwP3M3W25ziMxThUQIVsWSoosBM+g1kws/i+g4RrUmmYIedWIcB7zTrS8pyPPbz/4jwH4Bud7SkL4QFgj2D3IAu7MPWZqDRAiRMBz6SUH32CMJYojvSIFYW3JmwIk4nxTzBvyPMRF+GxAmNs1BLy5oZnhNlFuFrf26+8JdofdzCOhztHzmbfRg2SJueTLtaL1UeC8NhlGlQ12dFEVNy6ZT8vqhqYcB0HRcTVA15qwrozDJE11xEqzcCRIyq5coi4E4l71/DuTarxzZjEmHdaShaO2SWRyE/8xjBc7jEvlyv1OGMD2weRZv75RCK96fWEAqhzpyqieWUfvRRAljmHDc5jg+663kO9E+GAsaaDG+6q9zTQQ== maceacherndjh@gmail.com" ];
-  };
   #####################
   # System Applications
   #####################
   environment.systemPackages = with pkgs; [
     git
     vim
+    zsh
+    home-manager
+    htop
+    jq
+    ripgrep
+    graphviz
+    shfmt
+    shellcheck
+    xclip
   ];
+  environment.shells = [ pkgs.zsh ];
+  virtualisation.docker.enable = true;
+  #####################
+  # Users
+  #####################
+  users.users.davidmaceachern = {
+    description = "David MacEachern";
+    isNormalUser = true;
+    createHome = true;
+    home = "/home/davidmaceachern";
+    shell = pkgs.zsh;
+    extraGroups = [ "wheel" "networkmanager" "docker"];
+    openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDED+Kh6L67xD79Dzh/n3sjC5OLMba9xphRktLMm14xATI6aH4miXwI2kstn7bhy/16qcLthdv3hMvAooSy4kIfkWSa8MURGX/I5CUCgJrZuNH2KkUP+fqi7tcz9z6PS+0mAJR097rj7F+zDe+gDAibJ6vex7TMjyKVOmzNlaRlcJl2Bza0ddA5EJ3nJHXJGh0cvwTMVLw0JbRhKKz9s87gywVzhgBBVIZLXxu4Aliku91A5xCFPWWdEPXdkrH4RTN7qM2/bIGs6cUVtJ8lcEZU4+z6dhYb4Fu/z873aPrcoOp9ufsGQqCGWoobCIwP3M3W25ziMxThUQIVsWSoosBM+g1kws/i+g4RrUmmYIedWIcB7zTrS8pyPPbz/4jwH4Bud7SkL4QFgj2D3IAu7MPWZqDRAiRMBz6SUH32CMJYojvSIFYW3JmwIk4nxTzBvyPMRF+GxAmNs1BLy5oZnhNlFuFrf26+8JdofdzCOhztHzmbfRg2SJueTLtaL1UeC8NhlGlQ12dFEVNy6ZT8vqhqYcB0HRcTVA15qwrozDJE11xEqzcCRIyq5coi4E4l71/DuTarxzZjEmHdaShaO2SWRyE/8xjBc7jEvlyv1OGMD2weRZv75RCK96fWEAqhzpyqieWUfvRRAljmHDc5jg+663kO9E+GAsaaDG+6q9zTQQ== maceacherndjh@gmail.com" ];
+  };
 }
